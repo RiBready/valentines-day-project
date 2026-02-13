6c722de57965d533188bed0fd87bde764deb97ae
Riley Ty <ttyy0002@student.monash.edu>
2026-02-13 18:25:45 +1100

    i thought i pushed this through already

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ ctk.set_appearance_mode("Light")
 BG_COLOUR = "#e0a7ff"
 DIALOGUE_BOX ="#faacd4"
 BORDER_COLOUR = "#a6e9ff"
-TEXT_COLOUR = ""
+TEXT_COLOUR = "#FF4C4F"
+BTN_TEXT_COLOUR = "#FF9799"
+BTN_COLOUR = "#FF69B4"
+BTN_HOVER = "#FAFFB6"
 
 class ValentineApp(ctk.CTk):
+    def say_yes(self):
+            print("She said yes!") # Dummy message for now
+
     def __init__(self):
         super().__init__()
         # Square window
@@ -26,6 +32,21 @@ class ValentineApp(ctk.CTk):
             )
         # Dialogue Box frame layout manager (Pack)
         self.game_frame.pack(padx=20, pady=20, fill="both", expand=True)
+
+        # The big question
+        self.label = ctk.CTkLabel(
+            self.game_frame, text="Will you be my valentine?", font=("VCR OSD MONO", 20), text_color=TEXT_COLOUR
+        )
+        self.label.pack(pady=50)
+
+        # Yes button
+        self.yes_btn = ctk.CTkButton(
+            self.game_frame, text="Yes!", command=self.say_yes, font=("VCR OSD MONO", 16), fg_color=BTN_COLOUR,
+            hover_color=BTN_HOVER, text_color=BTN_TEXT_COLOUR, corner_radius=0, width=140, height=50
+        )
+
+        self.yes_btn.place(x=50, y=384)
+
         
 
 if __name__ == "__main__":
