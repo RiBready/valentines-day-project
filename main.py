32b72273b661eb7235e93d13fa0231bbe7b8e81b
Riley Ty <ttyy0002@student.monash.edu>
2026-02-13 21:00:57 +1100

    refactoring main code as there'll be several screens to this.
    created no button and a dummy "no" message (via a function), will be changed later on.
    
    created different functions to start classes, still yet to create the yes and no pages.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import customtkinter as ctk
 
 # -- Config! --
-# To force light mode for pastel colours
-ctk.set_appearance_mode("Light")
+ctk.set_appearance_mode("Light") # To force light mode for pastel colours
 BG_COLOUR = "#e0a7ff"
 DIALOGUE_BOX ="#faacd4"
 BORDER_COLOUR = "#a6e9ff"
@@ -12,8 +11,12 @@ BTN_COLOUR = "#FF69B4"
 BTN_HOVER = "#FAFFB6"
 
 class ValentineApp(ctk.CTk):
+    
     def say_yes(self):
             print("She said yes!") # Dummy message for now
+    
+    def say_no(self):
+         print("how did you get here.")
 
     def __init__(self):
         super().__init__()
@@ -33,19 +36,48 @@ class ValentineApp(ctk.CTk):
         # Dialogue Box frame layout manager (Pack)
         self.game_frame.pack(padx=20, pady=20, fill="both", expand=True)
 
-        # The big question
+        self.create_start_page()
+    
+    def clear_screen(self):
+         # When called, it loops through each widget in the window and destroys it.
+         for widget in self.winfo_children():
+              widget.destroy()
+            
+    def create_start_page(self):
+         # The big question
         self.label = ctk.CTkLabel(
-            self.game_frame, text="Will you be my valentine?", font=("VCR OSD MONO", 20), text_color=TEXT_COLOUR
+            self.game_frame, text="Will you be my valentine?",
+            font=("VCR OSD MONO", 28), text_color=TEXT_COLOUR
         )
         self.label.pack(pady=50)
 
         # Yes button
         self.yes_btn = ctk.CTkButton(
-            self.game_frame, text="Yes!", command=self.say_yes, font=("VCR OSD MONO", 16), fg_color=BTN_COLOUR,
-            hover_color=BTN_HOVER, text_color=BTN_TEXT_COLOUR, corner_radius=0, width=140, height=50
+            self.game_frame, text="Yes!", command=self.say_yes,
+            font=("VCR OSD MONO", 20), fg_color=BTN_COLOUR,
+            hover_color=BTN_HOVER, text_color=BTN_TEXT_COLOUR,
+            corner_radius=0, width=140, height=50
         )
+        self.yes_btn.place(x=30, y=384)
 
-        self.yes_btn.place(x=50, y=384)
+        # No button
+        self.no_btn = ctk.CTkButton(
+            self.game_frame, text="No :((", command=self.say_no,
+            font=("VCR OSD MONO", 20), fg_color=BTN_COLOUR,
+            hover_color=BTN_HOVER, text_color=BTN_TEXT_COLOUR,
+            corner_radius=0, width=140, height=50
+        )
+        self.no_btn.place(x=290, y=384)
+
+    def create_yes_page(self):
+         self.clear_screen()
+         # TODO: make a success frame
+         pass
+
+    def create_no_page(self):
+         self.clear_screen
+         #TODO: make sad frame
+         pass
 
         
 
